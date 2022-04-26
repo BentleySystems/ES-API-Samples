63b10c7454e95126277f9d86ba35d65de3375d1a
Xellon <19504901+Xellon@users.noreply.github.com>
2022-04-26 12:41:34 +0300

    Cleaned up code

--- EsApiProjectsSampleApp/Program.cs
+++ EsApiProjectsSampleApp/Program.cs
@@ -31,7 +31,7 @@ await ConsoleApp.RunAsync(args, async (arguments, configuration) =>
     }
     
     var billingCountries = await billingCountriesResponse.Content.ReadFromJsonAsync<PagedResponse<string>>();
-    var billingCountry = (billingCountries?.Items ?? Enumerable.Empty<string>()).First();
+    var billingCountry = billingCountries?.Items.FirstOrDefault() ?? throw new Exception("Could not find any billing countries");
 
     ConsoleApp.Log("Will use '{0}' billing country", billingCountry);
 
@@ -41,7 +41,7 @@ await ConsoleApp.RunAsync(args, async (arguments, configuration) =>
     // Not handling possible network or status code errors for simplicity's sake
     var dataCenters = await client.GetFromJsonAsync<PagedResponse<string>>(
         $"/project/v1/projectTypes/{configuration.ProjectType}/datacenters");
-    var dataCenter = (dataCenters?.Items ?? Enumerable.Empty<string>()).First();
+    var dataCenter = dataCenters?.Items.FirstOrDefault() ?? throw new Exception("Could not find any data centers");
 
     ConsoleApp.Log("Will use '{0}' data center", dataCenter);
 
@@ -58,15 +58,13 @@ await ConsoleApp.RunAsync(args, async (arguments, configuration) =>
     // Create project
     ConsoleApp.Log($"Starting project '{arguments.Name}' creation");
 
-    var createProject = new CreateProject()
-    {
-        TemplateId = template.Id,
-        DisplayName = arguments.Name,
-        Number = arguments.Name,
-        DataCenterLocation = dataCenter,
-        BillingCountry = billingCountry,
-    };
-    var createProjectResponse = await client.PostAsync("project/v1/projects", JsonContent.Create(createProject));
+    var createProject = new CreateProject(
+        TemplateId: template.Id,
+        DisplayName: arguments.Name,
+        Number: arguments.Name,
+        DataCenterLocation: dataCenter,
+        BillingCountry: billingCountry);
+    var createProjectResponse = await client.PostAsJsonAsync("project/v1/projects", createProject);
 
     // Handling create project request because it contains custom status codes:
     //  - 422 - when provided model is incorrect (like non-existent data center location or billing country)
@@ -86,20 +84,16 @@ await ConsoleApp.RunAsync(args, async (arguments, configuration) =>
     // Wait for create project provision to finish
     ConsoleApp.Log("Waiting for new project to be provisioned");
 
-    string provisionState;
-    do
-    {
-        var status = await client.GetFromJsonAsync<ProvisionStatus>($"project/v1/projects/{projectId}/provisions/{provisionId}");
-        
-        provisionState = status?.State ?? throw new Exception("Service could not retrieve provision state");
-        
-        ConsoleApp.Log("Provision status: {0}", provisionState);
-        if (provisionState is "Queued" or "Created" or "Started")
-        {
-            await Task.Delay(TimeSpan.FromSeconds(5));
-        }
+    async Task<string?> GetProvisionStateAsync() =>
+        (await client.GetFromJsonAsync<ProvisionStatus>($"project/v1/projects/{projectId}/provisions/{provisionId}"))
+        ?.State;
 
-    } while (provisionState is "Queued" or "Created" or "Started");
+    string? provisionState;
+    while ((provisionState = await GetProvisionStateAsync()) is CopyState.Queued or CopyState.Created or CopyState.Started)
+    {
+        ConsoleApp.Log("Provision status: {0}", provisionState);
+        await Task.Delay(TimeSpan.FromSeconds(5));
+    }
 
     ConsoleApp.Log("Provisioning finished. Fetching project.");
 
@@ -116,7 +110,7 @@ await ConsoleApp.RunAsync(args, async (arguments, configuration) =>
     ConsoleApp.Log("Fetching created project list.");
 
     var projectsResponse = await client.GetFromJsonAsync<PagedResponse<Project>>($"project/v1/projects?projectName={arguments.Name}");
-    var projects = (projectsResponse?.Items ?? Enumerable.Empty<Project>());
+    var projects = projectsResponse?.Items ?? throw new Exception("Could not parse GET projects response.");
 
     // Print projects to console
     ConsoleApp.Log("Project names:");
