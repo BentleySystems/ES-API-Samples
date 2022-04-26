fa342252dca266b857c823479f72dd5cba4aa909
Xellon <19504901+Xellon@users.noreply.github.com>
2022-04-26 14:43:53 +0300

    Added copyrights

--- EsApiProjectsSampleApp/Program.cs
+++ EsApiProjectsSampleApp/Program.cs
@@ -1,4 +1,8 @@
-﻿using System.Net;
+﻿/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+using System.Net;
 using System.Net.Http.Json;
 using EsApiProjectsSampleApp;
 
