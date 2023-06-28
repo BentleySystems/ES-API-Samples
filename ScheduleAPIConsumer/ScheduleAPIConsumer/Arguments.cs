a2fc0692c30f41d9c5d7c0f7650faf71be6795de
Connor Stephen <connor.stephen@bentley.com>
2023-06-28 13:56:36 +0100

    adds 4D Schedule API Consumer to ES-API Samples repo

---
+++ ScheduleAPIConsumer/ScheduleAPIConsumer/Arguments.cs
@@ -0,0 +1,8 @@
+﻿/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+namespace ScheduleAPIConsumer
+{
+    public record Arguments(string Token, string Schedule, bool All, bool Single, bool Post);
+}
