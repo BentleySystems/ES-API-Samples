a284d38862c9dc60a002d0e8f9759583063dc001
cobenatben <126157345+cobenatben@users.noreply.github.com>
2023-08-10 09:44:38 +0100

    Adds SPA sample app to the repository (#12)
    * adds 4D Schedule API Consumer to ES-API Samples repo
    
    * Clarifies usage in the README
    
    * Clears up wording
    
    * removes option to query all change requests as this endpoint no longer exists. updates POST request to be consistent with API, and further clarification added to README
    
    * moves .github back to top level
    
    * dotnet.yml targets proper folder now
    
    * removes --all option and runs a workflow to get and change a Resource Status History item and show the subsequent change request ID instead. additionally changes the appsettings.json to make the queries in Program.cs less convoluted when writing
    
    * first step to rename project to EsApi4DScheduleSampleApp
    
    * renames and moves files to new renamed folder
    
    * moves LICENSE.md to root
    
    * creates a basic README.md in the root folder and edits the subfolder READMEs with redundant information removed.
    
    * ServiceHost returned back to es-api rather than dev
    
    * adds build for Schedule app
    
    * changes to resolve comments related to functionality on PR. this includes using JSON (de)serialization instead of manual string manipulation
    
    * resolves comments on PR by adding relative links
    
    * correct WorkArea API description added
    
    * alters Project sample app readme
    
    * adds SPA sample app
    
    * edits default readme
    
    * adds 4D Schedule API Consumer to ES-API Samples repo
    
    * Clarifies usage in the README
    
    * Clears up wording
    
    * removes option to query all change requests as this endpoint no longer exists. updates POST request to be consistent with API, and further clarification added to README
    
    * moves .github back to top level
    
    * dotnet.yml targets proper folder now
    
    * removes --all option and runs a workflow to get and change a Resource Status History item and show the subsequent change request ID instead. additionally changes the appsettings.json to make the queries in Program.cs less convoluted when writing
    
    * first step to rename project to EsApi4DScheduleSampleApp
    
    * renames and moves files to new renamed folder
    
    * moves LICENSE.md to root
    
    * creates a basic README.md in the root folder and edits the subfolder READMEs with redundant information removed.
    
    * ServiceHost returned back to es-api rather than dev
    
    * adds build for Schedule app
    
    * changes to resolve comments related to functionality on PR. this includes using JSON (de)serialization instead of manual string manipulation
    
    * resolves comments on PR by adding relative links
    
    * correct WorkArea API description added
    
    * alters Project sample app readme
    
    * adds SPA sample app
    
    * edits default readme
    
    * resolving comments from pull request

---
+++ EsApi4DScheduleSPASampleApp/src/App.tsx
@@ -0,0 +1,232 @@
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import { BrowserAuthorizationClient } from "@itwin/browser-authorization";
+import "./App.css";
+import jwt_decode from "jwt-decode";
+
+const App: React.FC = () => {
+  const [windowUrl, setWindowUrl] = useState(new URL(window.location.href));
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [userClientId, setClientId] = useState("");
+  const [scheduleId, setScheduleId] = useState("");
+  const [getResponse, setGetResponse] = useState({ items: [] });
+  const [postResponse, setPostResponse] = useState({ changeRequestId: "" });
+  const [changeResponse, setChangeResponse] = useState({
+    id: "",
+    status: "",
+    failureDescription: "",
+  });
+  const [token, setToken] = useState<string>("");
+
+  useEffect(() => {
+    handle(windowUrl);
+  }, [windowUrl]);
+
+  const url =
+    "https://es-api.bentley.com/4dschedule/v1/schedules/" +
+    scheduleId +
+    "/resourceStatusHistory";
+
+  const authClient = new BrowserAuthorizationClient({
+    clientId: userClientId,
+    scope: "enterprise offline_access",
+    redirectUri: "https://localhost:3000/signin-callback",
+    postSignoutRedirectUri: "https://localhost:3000/logout",
+    responseType: "code",
+    silentRedirectUri: "https://localhost:3000/signin-callback",
+    authority: "https://ims.bentley.com/",
+  });
+
+  const login = async () => {
+    try {
+      await authClient.signInPopup();
+      setIsLoggedIn(authClient.isAuthorized);
+      const tok = await authClient.getAccessToken();
+      setToken(tok);
+    } catch (error: any) {
+      alert(error);
+      console.log("error in login: " + error);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      await authClient.signOutRedirect();
+    } catch {
+      await authClient.signOut();
+    }
+  };
+
+  const obtainNewToken = async () => {
+    await authClient.signInSilent();
+    await authClient.handleSigninCallback();
+    const tok = await authClient.getAccessToken();
+    setToken(tok);
+    return tok;
+  };
+
+  const checkTokenExpired = async () => {
+    const decodedToken = (await jwt_decode(token)) as { exp: number };
+    if (Date.now() >= decodedToken.exp * 1000) {
+      const newTok = await obtainNewToken();
+      return newTok;
+    } else {
+      return token;
+    }
+  };
+
+  const handle = async (url: URL) => {
+    if (url.pathname === "/signin-callback") {
+      try {
+        await authClient.handleSigninCallback();
+      } catch (err: any) {
+        alert("error in handle: " + err);
+      }
+    }
+  };
+
+  const getRequest = async () => {
+    const tok = await checkTokenExpired();
+    getFunction(tok);
+  };
+
+  const getFunction = (tok) => {
+    const config = {
+      headers: { Authorization: tok },
+    };
+    Axios.get(url, config)
+      .then((axiosResponse) => {
+        setGetResponse(axiosResponse.data);
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("Error in GET: " + error.message);
+      });
+  };
+
+  const postRequest = async () => {
+    const tok = await checkTokenExpired();
+    postFunction(tok);
+  };
+
+  const postFunction = (tok) => {
+    const config = {
+      headers: { Authorization: tok },
+    };
+    const body = {
+      changeRequestId: crypto.randomUUID,
+      item: {
+        resourceId: getResponse.items[0].resourceId,
+        date: "2023-07-06T14:20:57.774Z",
+        statusCategoryId: getResponse.items[0].statusCategoryId,
+        statusItemId: getResponse.items[0].statusItemId,
+      },
+    };
+    Axios.post(url, body, config)
+      .then((axiosResponse) => {
+        setPostResponse(axiosResponse.data);
+      })
+      .catch((error) => {
+        alert("Error in POST: " + error.message);
+      });
+  };
+
+  const changeRequest = async () => {
+    const tok = await checkTokenExpired();
+    changeFunction(tok);
+  };
+
+  const changeFunction = (tok) => {
+    const config = {
+      headers: { Authorization: tok },
+    };
+    const changeRequestUrl =
+      "https://es-api.bentley.com/4dschedule/v1/schedules/" +
+      scheduleId +
+      "/changeRequests/" +
+      postResponse.changeRequestId;
+    Axios.get(changeRequestUrl, config)
+      .then((axiosResponse) => {
+        setChangeResponse(axiosResponse.data);
+      })
+      .catch(function (error) {
+        alert("Error in changeRequests GET: " + error.message);
+      });
+  };
+
+  const setSchedule = (event) => {
+    setScheduleId(event.target.value);
+  };
+
+  const setId = (event) => {
+    setClientId(event.target.value);
+  };
+
+  const renderLogin = () => {
+    return (
+      <>
+        <h3>Logged in at {userClientId}</h3>
+        <button onClick={logout}>Logout</button>
+        <p>Set your schedule ID</p>
+        <input id="scheduleId" onChange={setSchedule} value={scheduleId} />
+        <br />
+        <h4>Send GET to resourceStatusHistory endpoint</h4>
+        <button disabled={scheduleId.length < 1} onClick={getRequest}>
+          GET
+        </button>
+        <br />
+        <br />
+        <h5>GET Response:</h5>
+        <textarea readOnly value={JSON.stringify(getResponse)} />
+        <h4>Send POST to resourceStatusHistory endpoint</h4>
+        <button
+          disabled={scheduleId.length < 1 || getResponse.items.length < 1}
+          onClick={postRequest}
+        >
+          POST
+        </button>
+        <br />
+        <br />
+        <h5>POST Response:</h5>
+        <textarea readOnly value={JSON.stringify(postResponse)} />
+        <h4>Send GET to changeRequests endpoint</h4>
+        <button
+          disabled={
+            scheduleId.length < 1 || postResponse.changeRequestId === ""
+          }
+          onClick={changeRequest}
+        >
+          GET
+        </button>
+        <br />
+        <br />
+        <h5>GET Response:</h5>
+        <textarea readOnly value={JSON.stringify(changeResponse)} />
+      </>
+    );
+  };
+
+  const renderLogOut = () => {
+    return (
+      <>
+        <h3>Logged out</h3>
+        <p>Enter your client ID</p>
+        <input id="clientId" onChange={setId} value={userClientId} />
+        <br />
+        <button disabled={userClientId.length < 1} onClick={login}>
+          Login
+        </button>
+      </>
+    );
+  };
+
+  return (
+    <>
+      <h2>Sample SPA App interfacing with 4D Schedules External API</h2>
+      {<p>{token}</p>}
+      {isLoggedIn ? renderLogin() : renderLogOut()}
+    </>
+  );
+};
+
+export default App;
