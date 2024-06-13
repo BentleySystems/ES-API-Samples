4d14c956c5b4b5f60b851cd37462e256a37fd3d6
Connor Stephen <126157345+cobenatben@users.noreply.github.com>
2024-06-13 15:01:16 +0100

    removing unnecessary using directive

--- EsApi4DScheduleServiceSampleApp/EsApi4DScheduleSampleApp/Program.cs
+++ EsApi4DScheduleServiceSampleApp/EsApi4DScheduleSampleApp/Program.cs
@@ -3,7 +3,6 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 using System.Net;
-using System.Text.Json;
 using EsApi4DScheduleSampleApp;
 using EsApi4DScheduleSampleApp.Models;
 
