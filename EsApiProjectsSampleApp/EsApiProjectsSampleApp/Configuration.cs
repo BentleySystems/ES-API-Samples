e7c952a546fc32b981fdedc95303b25087bd9dbb
Connor Stephen <126157345+cobenatben@users.noreply.github.com>
2024-10-02 09:57:26 +0100

    removing projects as it is deprecated

--- EsApiProjectsSampleApp/EsApiProjectsSampleApp/Configuration.cs
+++
@@ -1,8 +0,0 @@
-﻿/*---------------------------------------------------------------------------------------------
-* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
-* See LICENSE.md in the project root for license terms and full copyright notice.
-*--------------------------------------------------------------------------------------------*/
-namespace EsApiProjectsSampleApp
-{
-    public record Configuration(Uri ServiceHost, string ProjectType = ProjectTypes.ProjectWise);
-}
